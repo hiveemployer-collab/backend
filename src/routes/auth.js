b1ab1ab2cae510a281db38b7e9e18f7a079b84ae
dyevinsilva <dyevinsilva@gmail.com>
2026-03-30 22:06:40 +1100

    fix missing User import in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 const { protect: auth } = require('../middleware/auth');
 const { sendPasswordResetEmail, sendWelcomeEmail, sendVerificationEmail } = require('../config/emailService');
 
